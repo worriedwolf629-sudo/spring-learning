f5ba203f81d92d7080888b31149e083b9090e212
Arish <Worriedwolf629@gmail.com>
2026-08-19 01:31:49 +0530

    Add Spring learning projects

---
+++ TRY1/src/main/java/org/example/PAYMENT.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class PAYMENT {
+    public void add() {
+        System.out.println("done");
+    }
+}
---
+++ demo/src/main/java/com/example/demo/hellocontroller.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.example.PAYMENT;
+
+
+public class hellocontroller {
+
+    public static void main() {
+
+
+        PAYMENT P = new PAYMENT();
+        P.add();  // another project file
+
+
+    }
+}
