251ba9964438d9b2244fbb50a80e6bf24c444942
Arish <Worriedwolf629@gmail.com>
2026-08-21 02:17:29 +0530

    Add springcoredemo project

---
+++ springcoredemo/demo/src/main/java/springcoredemo/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package springcoredemo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
