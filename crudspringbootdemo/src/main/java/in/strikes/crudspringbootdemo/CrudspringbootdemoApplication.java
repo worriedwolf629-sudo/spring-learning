44aabea8d0b029a84c9018a161d1d722bb4ffc51
Arish <Worriedwolf629@gmail.com>
2026-08-22 01:45:49 +0530

    add crudspringbootdemoproject

---
+++ crudspringbootdemo/src/main/java/in/strikes/crudspringbootdemo/CrudspringbootdemoApplication.java
@@ -0,0 +1,13 @@
+package in.strikes.crudspringbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudspringbootdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudspringbootdemoApplication.class, args);
+	}
+
+}
---
+++ crudspringbootdemo/src/test/java/in/strikes/crudspringbootdemo/CrudspringbootdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package in.strikes.crudspringbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudspringbootdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
