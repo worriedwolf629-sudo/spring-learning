cbcc4ab0c9cd1117c0ce285b22ad28a685bf0ca7
Arish <Worriedwolf629@gmail.com>
2026-08-21 22:20:09 +0530

    i

--- TRY1/src/main/java/org/example/Main.java
+++ TRY1/src/main/java/org/example/Main.java
@@ -13,5 +13,5 @@ public class Main {
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             IO.println("i = " + i);
         }
-    }
+    }//checking lol
 }
