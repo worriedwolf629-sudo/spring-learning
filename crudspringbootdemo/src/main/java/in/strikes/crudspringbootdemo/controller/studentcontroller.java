79e80a75a69d86af2008e45bc990c0e98bdb1d7b
Arish <Worriedwolf629@gmail.com>
2026-08-26 01:35:00 +0530

    initial commit

--- crudspringbootdemo/src/main/java/in/strikes/crudspringbootdemo/controller/studentcontroller.java
+++ crudspringbootdemo/src/main/java/in/strikes/crudspringbootdemo/controller/studentcontroller.java
@@ -2,31 +2,68 @@ package in.strikes.crudspringbootdemo.controller;
 
 import in.strikes.crudspringbootdemo.entity.students;
 import in.strikes.crudspringbootdemo.service.studentservice;
+import org.hibernate.annotations.NotFound;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/students")
 public class studentcontroller {
 
 
-    private studentservice studentservice;
+    private final studentservice studentservice;
 
     public studentcontroller(studentservice studentservice) {
         this.studentservice = studentservice;
     }
 
     @PostMapping("/create")
-    public ResponseEntity<students>createdstudent(@RequestBody students student) {
+    public ResponseEntity<students> createdstudent(@RequestBody students student) {
         System.out.println("student controller starteed ");
-        students createdstudent =studentservice.createdstudent(student);
+        students createdstudent = studentservice.createdstudent(student);
         System.out.println("student controller end ");
         return ResponseEntity.status(HttpStatus.CREATED).body(createdstudent);
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<students> getstudent(@PathVariable long id) {
+        students respstudent = studentservice.getstudent(id);
+        if (respstudent == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+        return ResponseEntity.ok(respstudent);
+
+    }
+
+    @GetMapping("getall")
+    public ResponseEntity<List<students>> getallstudent() {
+        List<students> studentlist = studentservice.getallstudent();
+        if (studentlist == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+        return ResponseEntity.ok(studentlist);
+
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<students> updatestudent(@PathVariable long id,@RequestBody students studentreq) {
+        students existingpstudent = studentservice.updatestudent(id, studentreq);
+        if (existingpstudent == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+        return ResponseEntity.ok(existingpstudent);
+    }
+
+    @DeleteMapping("delete/{id}")
+    public ResponseEntity<students> updatestudent(@PathVariable long id) {
+        students deletingpstudent = studentservice.deletestudent(id);
+        if (deletingpstudent == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+        return ResponseEntity.ok(deletingpstudent);
     }
 
 }
--- crudspringbootdemo/src/main/java/in/strikes/crudspringbootdemo/service/studentservice.java
+++ crudspringbootdemo/src/main/java/in/strikes/crudspringbootdemo/service/studentservice.java
@@ -3,6 +3,10 @@ package in.strikes.crudspringbootdemo.service;
 import in.strikes.crudspringbootdemo.entity.students;
 import in.strikes.crudspringbootdemo.repository.studentrepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class studentservice {
@@ -20,6 +24,39 @@ public class studentservice {
         return respstudents;
     }
 
+    public students getstudent(long id) {
+        Optional<students> respstudents = studentrepository.findById(id);
+        if (respstudents.isPresent()) {
+            return respstudents.get();
+        }
+        return null;
+    }
 
+    public List<students> getallstudent() {
+        List<students> studentslist = studentrepository.findAll();
+        return studentslist;
+    }
+
+    public students updatestudent(long id, @RequestBody students studentreq ) {
+        Optional<students> existingstudent = studentrepository.findById(id);
+        if (existingstudent.isEmpty()) {
+            return null;
+        }
+        students savenewdetails = existingstudent.get();
+        savenewdetails.setAge(studentreq.getAge());
+        savenewdetails.setName(studentreq.getName());
+        savenewdetails.setRollnum(studentreq.getRollnum());
+        savenewdetails.setSchool(studentreq.getSchool());
+        return studentrepository.save(savenewdetails);
+    }
+
+    public students deletestudent(long id) {
+        Optional<students> deletingstudent = studentrepository.findById(id);
+        if (deletingstudent.isPresent()) {
+            studentrepository.deleteById(id);
+            return deletingstudent.get();
+        }
+        return null;
+    }
 
 }
